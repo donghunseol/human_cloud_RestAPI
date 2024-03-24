cdec2a07f707e16e8d1c1ebf2503fa7b9c6cc811
SO YEON LEE <thdus1323@gmail.com>
2024-03-24 19:23:43 +0900

    좋아요 삭제 및 작성중

--- src/main/java/com/example/project_v2/reply/ReplyController.java
+++ src/main/java/com/example/project_v2/reply/ReplyController.java
@@ -1,11 +1,14 @@
 package com.example.project_v2.reply;
 
+import com.example.project_v2._core.errors.exception.Exception403;
+import com.example.project_v2._core.errors.exception.Exception404;
 import com.example.project_v2._core.util.ApiUtil;
 import com.example.project_v2.user.User;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +20,7 @@ public class ReplyController {
     private final ReplyService replyService;
     private final ReplyJPARepository replyJPARepository;
     private final HttpSession session;
+
     // 댓글 작성
     @PostMapping("/api/replies")
     public ResponseEntity<?> save(@RequestBody ReplyRequest.SaveDTO reqDTO) {
@@ -29,7 +33,17 @@ public class ReplyController {
     @DeleteMapping("/api/replies/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        replyService.댓글삭제(id,sessionUser.getId());
+        replyService.댓글삭제(id, sessionUser.getId());
         return ResponseEntity.ok(new ApiUtil<>(null));
+
+        @Transactional
+        public void 댓글삭제 ( int replyID, int sessionUserId){
+            Reply reply = replyJPARepository.findById(replyid)
+                    .orElseThrow(() -> new Exception404("없는 댓글을 삭제할 수 업어요"));
+            if (reply.getUser().getId() != SessionUserId) {
+                throw new Exception403("댓글을 삭제할 권한이 없어요");
+            }
+            replyJPARepository.deletedById(replyId);
+        }
     }
 }
