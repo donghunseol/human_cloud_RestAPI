51b7ca3e511cfba0b611d21e5fdb8ead83a54cec
seoldonghun <ehdgns5647@gmail.com>
2024-04-03 22:50:05 +0900

    jwt/sdh 좋아요 jwt 완

--- src/main/java/com/example/project_v2/love/LoveController.java
+++ src/main/java/com/example/project_v2/love/LoveController.java
@@ -3,6 +3,7 @@ package com.example.project_v2.love;
 import com.example.project_v2._core.util.ApiUtil;
 import com.example.project_v2.board.Board;
 import com.example.project_v2.board.BoardService;
+import com.example.project_v2.user.SessionUser;
 import com.example.project_v2.user.User;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,7 @@ public class LoveController {
     // 좋아요 입력
     @PostMapping("/api/loves/{id}")
     public ResponseEntity<?> save(@PathVariable Integer id, @RequestBody LoveRequest.SaveDTO reqDTO){
-        User sessionUser = (User) session.getAttribute("sessionUser");
+        SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
         Board board = boardService.findById(id);
         LoveResponse.DTO respDTO = loveService.save(reqDTO, sessionUser, board);
         return ResponseEntity.ok(new ApiUtil<>(respDTO));
@@ -29,7 +30,7 @@ public class LoveController {
     // 좋아요 삭제
     @DeleteMapping("/api/loves/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id){
-        User sessionUser = (User) session.getAttribute("sessionUser");
+        SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
         loveService.delete(id, sessionUser);
         return ResponseEntity.ok(new ApiUtil<>(null));
     }
