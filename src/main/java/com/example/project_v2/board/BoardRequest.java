a613564f5c9a565f8b358e0bb9dbf053dbe9c937
dongkipark1 <qkrehdrl4417@naver.com>
2024-04-03 15:47:49 +0900

    board aop 끝

--- src/main/java/com/example/project_v2/board/BoardRequest.java
+++ src/main/java/com/example/project_v2/board/BoardRequest.java
@@ -1,13 +1,17 @@
 package com.example.project_v2.board;
 
 import com.example.project_v2.user.User;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 public class BoardRequest {
     @Data
     public static class SaveDTO{
-        private Integer id; // board의 pk
+        @Size(min = 1, max = 20, message = "제목은 20자를 초과할 수 없습니다.")
+        @NotEmpty(message = "제목을 입력하셔야 합니다")
         private String title;
+        @NotEmpty(message = "내용을 입력하셔야 합니다")
         private String content;
 
         // 빌더 패턴
@@ -22,8 +26,10 @@ public class BoardRequest {
 
     @Data
     public static class UpdateDTO{
-        private Integer id; // board의 pk
+        @Size(min = 1, max = 20, message = "제목은 1자 미만, 20자를 초과할 수 없습니다.")
+        @NotEmpty(message = "제목을 입력하셔야 합니다")
         private String title;
+        @NotEmpty(message = "내용을 입력하셔야 합니다")
         private String content;
     }
 }
